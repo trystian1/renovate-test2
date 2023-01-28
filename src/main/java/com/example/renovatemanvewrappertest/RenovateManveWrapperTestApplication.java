1a622f10cd4a9b9682a21ebe783bfcd542cb44aa
tofferman <tofferman@bol.com>
2023-01-28 11:15:33 +0100

    main intitial commit

---
+++ src/main/java/com/example/renovatemanvewrappertest/RenovateManveWrapperTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.renovatemanvewrappertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenovateManveWrapperTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenovateManveWrapperTestApplication.class, args);
+	}
+
+}
